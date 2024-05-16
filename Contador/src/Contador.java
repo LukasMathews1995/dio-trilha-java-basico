2ed75f936b2f3e3385397458ba5a42d1d4f6304b
Lucas Mathews <160336363+LukasMathews1995@users.noreply.github.com>
2024-05-16 16:35:03 -0300

    Update Contador.java

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -6,6 +6,35 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args)  {
+
+             Scanner sc = new Scanner (System.in).useLocale(Locale.US);
+        System.out.println("Insira o primeiro parâmetro : ");
+        int parametroUm= sc.nextInt();
+        System.out.println("Insira o segundo parâmetro : ");
+        int parametroDois = sc.nextInt();
+        try {
+         
+          contar(parametroUm,parametroDois);
+        
+        }
+        
+        catch(ParametroInvalidoException exception){
+          System.err.println("O segundo parâmetro deve ser maior que o primeiro parâmetro");
+        }
+
+
+}
+static void contar(int parametroUm,int parametroDois) throws ParametroInvalidoException{
+  if(parametroUm>parametroDois){
+  throw new ParametroInvalidoException();
+  }
+  int contagem = parametroDois - parametroUm;
+  for(int i = 0 ; i<= contagem; i++){
+    System.out.println(i);
+  }
+
+}
     }
+    
   
   }
