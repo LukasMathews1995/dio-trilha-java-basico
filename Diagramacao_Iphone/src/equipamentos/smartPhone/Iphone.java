b4c9b50c498e0701b34a26e227a48790e6c70262
Lucas Mathews <160336363+LukasMathews1995@users.noreply.github.com>
2024-05-16 16:40:15 -0300

    commit_atualizado

--- Diagramacao_Iphone/src/equipamentos/smartPhone/Iphone.java
+++ Diagramacao_Iphone/src/equipamentos/smartPhone/Iphone.java
@@ -7,41 +7,44 @@ import equipamentos.reprodutormusica.ReprodutorMusical;
 public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
     @Override
     public void ligar() {
+        System.out.println("ligando");
 
     }
 
     @Override
     public void atender() {
-
+System.out.println("atendeu");
     }
 
     @Override
     public void iniciarCorreioVoz() {
+        System.out.println("iniciarCorreioVoz");
 
     }
 
     @Override
     public void exibirPagina() {
-
+        System.out.println("Exibindo a pagina");
     }
 
     @Override
     public void adicionarAba() {
+        System.out.println("adicionando a aba");
 
     }
 
     @Override
     public void tocar() {
-
+        System.out.println("tocando musica");
     }
 
     @Override
     public void pausar() {
+        System.out.println("pausou a musica");
 
     }
 
     @Override
     public void selecionarMusica() {
-
-    }
+        System.out.println("musica selecionada");
 }
